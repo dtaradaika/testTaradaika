a6079715893b0a1f757de6e77592193e7483ae91
Dmytro Taradaika <taradayka.d@gmail.com>
2022-08-10 12:11:19 +0200

    move pangesture to collectionview

--- TestTaradaika/TestTaradaika/ViewController.swift
+++ TestTaradaika/TestTaradaika/ViewController.swift
@@ -7,28 +7,21 @@
 
 import UIKit
 
-//class DataSource {
-//
-//    let lock = NSLock()
-//    var itemsCounter: Int = 0
-//
-//    func append() {
-//        lock.lock()
-//        itemsCounter = itemsCounter + 1
-//    }
-//
-//
-//}
-
 class ViewController: UIViewController {
 
     @IBOutlet var collectionView: UICollectionView!
     
     var itemsCounter: Int = 0
     
+    var cellCenter: CGPoint = .zero
+    var selectedCell: UICollectionViewCell?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(moveCell(gesture:)))
+        
+        collectionView.addGestureRecognizer(gesture)
     }
     
     public func addItem() {
@@ -54,7 +47,44 @@ class ViewController: UIViewController {
         addItem()
     }
 
-
+    @objc func moveCell(gesture: UIPanGestureRecognizer) {
+      
+        var translation: CGPoint = .zero
+        
+        if let selectedCell = selectedCell {
+            translation = gesture.translation(in: selectedCell)
+        }
+            
+        if gesture.state == .began {
+            
+            guard let indexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)),
+                  let cell = collectionView.cellForItem(at: indexPath) else {
+                return
+            }
+            
+            selectedCell = cell
+            cellCenter = cell.center
+        }
+        
+        selectedCell?.center = CGPoint(x: cellCenter.x, y: cellCenter.y + translation.y)
+        
+        if gesture.state == .cancelled {
+            selectedCell?.center = cellCenter
+        }
+        
+        if gesture.state == .ended {
+            if translation.y > 75 || translation.y < -75 {
+                
+                guard let cell = selectedCell, let index = collectionView.indexPath(for: cell) else {
+                    return
+                }
+                
+                removeItem(index: index.item)
+            } else {
+                selectedCell?.center = cellCenter
+            }
+        }
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
@@ -64,8 +94,14 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
         cell.backgroundColor = .red
+        
+        
+        //gesture.delegate = self
+        //cell.addGestureRecognizer(gesture)
+        
         return cell
     }
 }
@@ -75,6 +111,32 @@ extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         removeItem(index: indexPath.item)
     }
+}
+
+
+extension ViewController: UIGestureRecognizerDelegate {
+    
+//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return true
+//    }
+    
+//    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//       
+//    
+//        guard let panGesture = gestureRecognizer as? UIPanGestureRecognizer,
+//              let cellView = panGesture.view else {
+//            return false
+//        }
+//        
+//        let velocity = panGesture.velocity(in: cellView)
+//        
+//      //  if velocity.y > 10 {
+//            print( " > \(velocity.y)")
+//       // }
+//        
+//        return true
+//        
+//    }
 }
 
 
