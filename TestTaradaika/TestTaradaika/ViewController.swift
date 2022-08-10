4ec1c9c774a420897174aa22c949ac3e1be16bed
Dmytro Taradaika <taradayka.d@gmail.com>
2022-08-10 11:07:18 +0200

    add simple horizontal collectionview

--- TestTaradaika/TestTaradaika/ViewController.swift
+++ TestTaradaika/TestTaradaika/ViewController.swift
@@ -17,3 +17,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
+
